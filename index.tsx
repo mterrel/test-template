e7f3b3a8a7d8c24380ea70434122944d08a4ba48
Mark Terrel <mark@unbounded.systems>
2020-08-26 11:14:16 -0600

    null style

--- index.tsx
+++ index.tsx
@@ -1,36 +1,11 @@
-import Adapt, { Group, handle } from "@adpt/core";
-
-import { UrlRouter } from "@adpt/cloud/http";
-import { NodeService, ReactApp } from "@adpt/cloud/nodejs";
-import { Postgres } from "@adpt/cloud/postgres";
-import path from "path";
-import { k8sTestStyle } from "./styles";
+import Adapt from "@adpt/core";
+import { NodeService } from "@adpt/cloud/nodejs";
+import { aioCloud } from "./styles";
 
 const targetDir = process.env.TARGET_DIR;
 if (!targetDir) throw new Error(`TARGET_DIR must be defined`);
 
-function App() {
-    const pg = handle();
-    const app = handle();
-    const api = handle();
+const App = () => <NodeService srcDir={targetDir} />;
 
-    return <Group>
-
-        <UrlRouter
-            port={8080}
-            routes={[
-                { path: "/api/", endpoint: api },
-                { path: "/", endpoint: app }
-            ]} />
-
-        <ReactApp handle={app} srcDir={path.join(targetDir, "frontend")} />
-
-        <NodeService handle={api} srcDir={path.join(targetDir, "backend")} connectTo={pg} />
-
-        <Postgres handle={pg} />
-
-    </Group>;
-}
-
-Adapt.stack("default", <App />, k8sTestStyle());
+Adapt.stack("default", <App />, aioCloud);
 
