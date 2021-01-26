e14f2f426bfc2b4ba13c687ad0617160a8839dc3
Manish Vachharajani <manishv@unbounded.systems>
2021-01-26 13:50:56 -0700

    Move TARGET_DIR to ADAPTABLE_SOURCE_REPO

--- index.tsx
+++ index.tsx
@@ -2,10 +2,10 @@ import Adapt from "@adpt/core";
 import { NodeService } from "@adpt/cloud/nodejs";
 import { aioCloud } from "./styles";
 
-const targetDir = process.env.TARGET_DIR;
-if (!targetDir) throw new Error(`TARGET_DIR must be defined`);
+const srcDir = process.env.ADAPTABLE_SOURCE_REPO;
+if (!srcDir) throw new Error(`ADAPTABLE_SOURCE_REPO must be defined`);
 
-const App = () => <NodeService srcDir={targetDir} />;
+const App = () => <NodeService srcDir={srcDir} />;
 
 Adapt.stack("default", <App />, aioCloud);
 
